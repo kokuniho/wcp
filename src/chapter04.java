3863a047bc96a606ec6aad03444acc001229cab8
kokuniho <96281192+kokuniho@users.noreply.github.com>
2022-05-17 12:41:50 +0900

    chapter4

---
+++ src/chapter04.java
@@ -0,0 +1,13 @@
+
+public class chapter04 {
+	
+	public static void main(String[] args) {
+		final int RUCKY_NUMBER = 10;
+		String str = "Kuniho";
+		System.out.println(str);
+		str = "Happy";
+		System.out.println(str);
+	    System.out.println(RUCKY_NUMBER);	
+	}
+
+}
